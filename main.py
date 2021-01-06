e7e967f5db80caf9d3ba2e679e092fc98afecbd3
PETROVSKIY <profi905089@gmail.com>
2021-01-06 16:02:07 +0000

    убрал круг

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello, world!')
+from turtle import *
+t = Turtle()
+t.screen.setup(800, 800)
+t.shape("turtle")
+
+def sq_cr(side):
+
+    for i in range(4):
+        t.left(270)
+        t.fd(side)
+    t.left(180)
+    t.circle(side / 2, 360)
+
+sq_cr(250)
+t.screen.exitonclick()
+t.screen.mainloop()
